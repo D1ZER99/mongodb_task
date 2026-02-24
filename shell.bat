52a6ed6cbe53c211b009726c77dc6a63d5b53e32
D1ZER99 <dizervf@gmail.com>
2026-02-24 23:12:59 +0200

    Remove unneed bash files

--- shell.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Connecting to MongoDB shell...
-echo.
-docker exec -it mongodb_local mongosh -u admin -p password123 --authenticationDatabase admin
--- stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Stopping MongoDB container...
-docker-compose down
-echo.
-echo MongoDB container stopped.
-echo.
-pause
